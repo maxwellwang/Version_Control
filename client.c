a6a8be43d97b511722233d9eb88ced3f28120494
Maxwell Wang <maxwellwang01@gmail.com>
2020-04-16 23:57:54 -0400

    coded server.c, set up client.c commands

--- client.c
+++ client.c
@@ -4,14 +4,46 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <errno.h>
+#include <netinet/in.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
-	int sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock == -1) {
-		perror("Error");
+	// check arg count
+	if (argc < 3) {
+		printf("Error: Expected at least 3 args, received %d\n", argc);
 		return 1;
 	}
-	// translate ip address/host name into useful format
-	struct hostent* result = gethostbyname();
+	// determine command to execute
+	if (strcmp(argv[1], "configure") == 0) {
+	
+	} else if (strcmp(argv[1], "checkout") == 0) {
+	
+	} else if (strcmp(argv[1], "update") == 0) {
+	
+	} else if (strcmp(argv[1], "upgrade") == 0) {
+	
+	} else if (strcmp(argv[1], "commit") == 0) {
+	
+	} else if (strcmp(argv[1], "push") == 0) {
+	
+	} else if (strcmp(argv[1], "create") == 0) {
+	
+	} else if (strcmp(argv[1], "destroy") == 0) {
+	
+	} else if (strcmp(argv[1], "add") == 0) {
+	
+	} else if (strcmp(argv[1], "remove") == 0) {
+	
+	} else if (strcmp(argv[1], "currentversion") == 0) {
+	
+	} else if (strcmp(argv[1], "history") == 0) {
+	
+	} else if (strcmp(argv[1], "rollback") == 0) {
+	
+	} else {
+		printf("Error: Expected configure, checkout, update, uprade, commit, push, create, destroy, add, remove, currentversion,"
+			" history, or rollback, received %s\n", argv[1]);
+		return 1;
+	}
 	return 0;
 }
