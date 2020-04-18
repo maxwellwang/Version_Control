2737b6d2bebfdaa93ff8ae1d1bdd06b957fc3f01
scheng <scheng2019@gmail.com>
2020-04-18 14:37:03 -0400

    Client networking

--- client.c
+++ client.c
@@ -6,44 +6,99 @@
 #include <errno.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
+
+
+
+int configure(int argc, char* argv[]) {
+}
+int update(int argc, char* argv[]) {
+}
+int upgrade(int argc, char* argv[]) {
+}
+int commit(int argc, char* argv[]) {
+}
+int push(int argc, char* argv[]) {
+}
+int create(int argc, char* argv[]) {
+}
+int destroy(int argc, char* argv[]) {
+}
+int add(int argc, char* argv[]) {
+}
+int c_remove(int argc, char* argv[]) {
+}
+int currentversion(int argc, char* argv[]) {
+}
+int history(int argc, char* argv[]) {
+}
+int rollback(int argc, char* argv[]) {
+}
+
+
+
+//returns socket fd of stream, exits if fails
+int c_connect() { 
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (socket == -1) {
+    printf("Socket creation failed\n");
+    exit(0);
+  }
+  
+  struct sockaddr_in serverAddress;
+  bzero(&serverAddress, sizeof(serverAddress));
+  serverAddress.sin_family = AF_INET;
+  //TODO: read ip and port from a file, and maybe even hostname?
+  serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+  serverAddress.sin_port = htons(8080);
+
+  while (connect(sockfd, (struct sockaddr *) &serverAddress, sizeof(serverAddress))) {
+    printf("Connecting to server failed, attemping again in 3s\n");
+    sleep(3);
+  }
+  
+  return sockfd;
+}
 
 int main(int argc, char* argv[]) {
-	// check arg count
-	if (argc < 3) {
-		printf("Error: Expected at least 3 args, received %d\n", argc);
-		return 1;
-	}
-	// determine command to execute
-	if (strcmp(argv[1], "configure") == 0) {
-	
-	} else if (strcmp(argv[1], "checkout") == 0) {
-	
-	} else if (strcmp(argv[1], "update") == 0) {
-	
-	} else if (strcmp(argv[1], "upgrade") == 0) {
-	
-	} else if (strcmp(argv[1], "commit") == 0) {
-	
-	} else if (strcmp(argv[1], "push") == 0) {
-	
-	} else if (strcmp(argv[1], "create") == 0) {
-	
-	} else if (strcmp(argv[1], "destroy") == 0) {
-	
-	} else if (strcmp(argv[1], "add") == 0) {
-	
-	} else if (strcmp(argv[1], "remove") == 0) {
-	
-	} else if (strcmp(argv[1], "currentversion") == 0) {
-	
-	} else if (strcmp(argv[1], "history") == 0) {
-	
-	} else if (strcmp(argv[1], "rollback") == 0) {
-	
-	} else {
-		printf("Error: Expected configure, checkout, update, uprade, commit, push, create, destroy, add, remove, currentversion,"
-			" history, or rollback, received %s\n", argv[1]);
-		return 1;
-	}
-	return 0;
+  // check arg count
+  c_connect();
+  return 1;
+  if (argc < 3) {
+    printf("Error: Expected at least 3 args, received %d\n", argc);
+    return 1;
+  }
+  // determine command to execute
+  if (strcmp(argv[1], "configure") == 0) {
+    configure(argc, argv);
+  } else if (strcmp(argv[1], "checkout") == 0) {
+    checkout(argc, argv);
+  } else if (strcmp(argv[1], "update") == 0) {
+    configure(argc, argv);
+  } else if (strcmp(argv[1], "upgrade") == 0) {
+    upgrade(argc, argv);
+  } else if (strcmp(argv[1], "commit") == 0) {
+    commit(argc, argv);
+  } else if (strcmp(argv[1], "push") == 0) {
+    push(argc, argv);
+  } else if (strcmp(argv[1], "create") == 0) {
+    create(argc, argv);
+  } else if (strcmp(argv[1], "destroy") == 0) {
+    destroy(argc, argv);
+  } else if (strcmp(argv[1], "add") == 0) {
+    add(argc, argv);
+  } else if (strcmp(argv[1], "remove") == 0) {
+    c_remove(argc, argv);
+  } else if (strcmp(argv[1], "currentversion") == 0) {
+    currentversion(argc, argv);
+  } else if (strcmp(argv[1], "history") == 0) {
+    history(argc, argv);
+  } else if (strcmp(argv[1], "rollback") == 0) {
+    rollback(argc, argv);
+  } else {
+    printf("Error: Expected configure, checkout, update, uprade, commit, push, create, destroy, add, remove, currentversion,"
+	   " history, or rollback, received %s\n", argv[1]);
+    return 1;
+  }
+  return 0;
 }
--- server.c
+++ server.c
@@ -11,68 +11,71 @@
 #define CONNECTION_QUEUE_SIZE 10
 
 int main(int argc, char* argv[]) {
-	// check arg count
-	if (argc != 2) {
-		printf("Error: Expected 2 args, received %d\n", argc);
-		return 1;
-	}
-	// check port number inputted
-	int portNo = atoi(argv[1]);
-	if (portNo == 0 && argv[1][0] != '0') {
-		printf("Error: Expected port number to be an integer, received %s\n", argv[1]);
-		return 1;
-	}
-	if (portNo < INT_MIN || portNo > INT_MAX) {
-		printf("Error: Expected port number to be within integer range, received %d\n", portNo);
-		return 1;
-	}
-	// init socket file descriptor
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1) {
-		perror("Error");
-		return 1;
-	}
-	// make socketaddr structure
-	struct sockaddr_in serverAddress;
-	bzero((char *) &serverAddress, sizeof(serverAddress));
-	serverAddress.sin_family = AF_INET;
-	serverAddress.sin_addr.s_addr = INADDR_ANY;
-	serverAddress.sin_port = htons(portNo);
-	// bind name to socket
-	int status = bind(sockfd, (struct sockaddr*) &serverAddress, sizeof(serverAddress));
-	if (status == -1) {
-		perror("Error");
-		return 1;
-	}
-	// listen for connections
-	listen(sockfd, CONNECTION_QUEUE_SIZE);
-	// accept connections
-	struct sockaddr_in clientAddress;
-	int clientLength = sizeof(clientAddress);
-	int newsockfd = accept(sockfd, (struct sockaddr*) &clientAddress, &clientLength);
-	if (newsockfd == -1) {
-		perror("Error");
-		return 1;
-	}
-	// prepare buffer
-	char* buffer = (char*) malloc(256);
-	bzero(buffer, 256);
-	// read message from client
-	int numBytes = read(newsockfd, buffer, 255);
-	if (numBytes == -1) {
-		perror("Error");
-		return 1;
-	}
-	printf("Here is the message: %s\n",buffer);
-	// write message back
-	numBytes = write(newsockfd, "I got your message", 18);
-	if (numBytes == -1) {
-		perror("Error");
-		return 1;
-	}
-	// close socket file descriptor
-	close(sockfd);
-	// free buffer
-	free(buffer);
-	return 0;
+  // check arg count
+  if (argc != 2) {
+    printf("Error: Expected 2 args, received %d\n", argc);
+    return 1;
+  }
+  // check port number inputted
+  int portNo = atoi(argv[1]);
+  if (portNo == 0 && argv[1][0] != '0') {
+    printf("Error: Expected port number to be an integer, received %s\n", argv[1]);
+    return 1;
+  }
+  if (portNo < 0 || portNo > 65535) { 
+   printf("Error: Invalid port number, received %d\n", portNo);
+    return 1;
+  }
+  
+  // init socket file descriptor
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd == -1) {
+    printf("Socket creation failed\n");
+    return 1;
+  }
+  
+  // make socketaddr structure
+  struct sockaddr_in serverAddress;
+  bzero((char *) &serverAddress, sizeof(serverAddress));
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_addr.s_addr = INADDR_ANY;
+  serverAddress.sin_port = htons(portNo);
+  // bind name to socket
+  int status = bind(sockfd, (struct sockaddr*) &serverAddress, sizeof(serverAddress));
+  if (status == -1) {
+    printf("Error: Failed to bind to socket\n");
+    return 1;
+  }
+  // listen for connections
+  listen(sockfd, CONNECTION_QUEUE_SIZE);
+  // accept connections
+  struct sockaddr_in clientAddress;
+  int clientLength = sizeof(clientAddress);
+  int newsockfd = accept(sockfd, (struct sockaddr*) &clientAddress, &clientLength);
+  if (newsockfd == -1) {
+    printf("Error: Failed to accept new connection\n");
+    return 1;
+  }
+  
+  // prepare buffer
+  char* buffer = (char*) malloc(256);
+  bzero(buffer, 256);
+  // read message from client
+  int numBytes = read(newsockfd, buffer, 255);
+  if (numBytes == -1) {
+    perror("Error");
+    return 1;
+  }
+  printf("Here is the message: %s\n",buffer);
+  // write message back
+  numBytes = write(newsockfd, "I got your message", 18);
+  if (numBytes == -1) {
+    perror("Error");
+    return 1;
+  }
+  // close socket file descriptor
+  close(sockfd);
+  // free buffer
+  free(buffer);
+  return 0;
 }
